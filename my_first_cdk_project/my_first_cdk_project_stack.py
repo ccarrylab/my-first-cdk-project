c1f654c22f1225949f68c174f498f82c703ae4a7
ccarrylab <cohen.carryl@gmail.com>
2020-06-08 19:55:55 -0400

    Before destroying

--- my_first_cdk_project/my_first_cdk_project_stack.py
+++ my_first_cdk_project/my_first_cdk_project_stack.py
@@ -1,21 +1,41 @@
-from aws_cdk import (
-    aws_s3 as _s3,
-    core
+from aws_cdk import(
+ aws_s3 as _s3, 
+ core
 )
 
 
 class MyFirstCdkProjectStack(core.Stack):
-
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
         # The code that defines your stack goes here
 
+        
         _s3.Bucket(
             self,
             "myBucketId",
             bucket_name="myfirstcdkproject021",
-            versioned=True,
-            encryption=_s3.BucketEncryption.KMS_MANAGED
-
+            versioned=False,
+            encryption=_s3.BucketEncryption.S3_MANAGED,
+            block_public_access=_s3.BlockPublicAccess.BLOCK_ALL,
         )
+
+        mybucket = _s3.Bucket(
+         self,
+         "myBucketId1"
+        )
+
+        snstopicname="abcxyz1234"
+
+        if not core.Token.is_unresolved(snstopicname) and len(snstopicname) > 10:
+            raise ValueError("Maximum value can be only 10 characters")
+
+        print(mybucket.bucket_name)
+     
+        output1 = core.CfnOutput(
+            self,
+            "myBucketOutput1",
+            value=mybucket.bucket_name,
+            description=f"My first CDK Bucket",
+            export_name="myBucketOutput1"
+     )
